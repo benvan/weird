bd29db777837f51f3fa05fbc15b4246268883a9b
Ben van Enckevort <github@benvan.co.uk>
2015-05-02 18:21:07 +0100

    gitignore

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var app = express();
+var Q = require('q');
+var fs = require('fs');
 
 app.use(require('express-promise')());
-
 app.use(express.static(__dirname));
 
-app.get('/foo', function(req, res) {
-  res.json({
-    result: 'ok'
-  });
+var random = function(arr){
+  return arr[Math.floor(Math.random()*arr.length)]
+};
+
+var buildQuestions = function(){
+  var text = fs.readFileSync('questions').toString();
+  var lines = text.split('\n');
+  var makeQuestion = function(line){
+    var pieces = line.split('|').map(function(x){ return x.trim() });
+    return {
+      question:pieces[0],
+      options:pieces.slice(1)
+    };
+  };
+  return lines.map(makeQuestion); 
+};
+
+var questions = buildQuestions();
+
+app.get('/next', function(req, res) {
+  res.json(random(questions));
 });
 
 app.listen(9000);
